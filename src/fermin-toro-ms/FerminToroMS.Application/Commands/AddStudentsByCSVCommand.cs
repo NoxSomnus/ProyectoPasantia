949bbc30ff948e0f67c59bc501ca56e61ca4c753
Diego <soniikkuc@hotmail.com>
2023-08-08 16:25:04 -0400

    Añadido command para agregar una lista de estudiantes a BD

---
+++ src/fermin-toro-ms/FerminToroMS.Application/Commands/AddStudentsByCSVCommand.cs
@@ -0,0 +1,19 @@
+﻿using FerminToroMS.Application.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FerminToroMS.Application.Commands
+{
+    public class AddStudentsByCSVCommand : IRequest<bool>
+    {
+        public AddStudentsByCSVRequest _request { get; set; }
+        public AddStudentsByCSVCommand(AddStudentsByCSVRequest request)
+        {
+            _request = request;
+        }
+    }
+}
